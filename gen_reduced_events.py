962e69315728210b44765074e857e60a207814e4
bkornyei <kornyei13@gmail.com>
2022-10-22 18:02:39 +0200

    FEAT Reduced by customer

---
+++ gen_reduced_events.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+CUSTOMER_START_INDEX = 0
+CUSTOMER_END_INDEX = 10000
+
+df = pd.read_csv("./data/gen/events.csv")
+df['customer'] = df['customer'].astype(str)
+df = df.sort_values(by='customer')
+
+customers = df['customer'].unique()[CUSTOMER_START_INDEX:CUSTOMER_END_INDEX]
+print(customers)
+
+df_reduced = df[df['customer'].isin(customers)]
+df_reduced.to_csv("./data/gen/reduced_events.csv", sep=',', index=False)
